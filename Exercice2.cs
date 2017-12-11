bb817a60fd8fff01bf5a495cb5a8c167fc6e7464
Billel Benzaid <benzaidb@gmail.com>
2017-12-10 22:04:50 -0500

    Exercice-2_Etape-2

--- Exercice2.cs
+++ Exercice2.cs
@@ -36,9 +36,22 @@ namespace Exercice2
                     st_read_input = Console.ReadLine(); // lire l'article
                 }
 
-                // Déterminer le coût total des fruits
                 Console.WriteLine();
-                Console.Write("Le coût total de vos achats = " + ShoppingCart(panier) + "$" + "\n\n");
+                Console.Write("Voulez vous appliquer la promotion ? <O> pour OUI, <N> pour NON: ");
+                st_read_input = Console.ReadLine();
+
+                // gestion des erreurs de frappe
+                while (!st_read_input.ToLower().Equals("o") & !st_read_input.ToLower().Equals("n"))
+                {
+                    Console.WriteLine();
+                    Console.Write("SVP, appuyer sur <O> ou sur <N> puis <Entrer>: ");
+                    st_read_input = Console.ReadLine();
+                }
+
+                // Déterminer le coût total des fruits, ceci dépend de si la promotation est appliquée ou pas
+                double c_total = st_read_input.Equals("o") ? ShoppingCart(panier, true) : ShoppingCart(panier, false);
+                Console.WriteLine();
+                Console.Write("Le coût total de vos achats = " + c_total + "$" + "\n\n");
                 Console.Write("Appuyer sur <Entrer> pour recommencer ...\n\n");
 
             } while (Console.ReadKey().Key == ConsoleKey.Enter);
@@ -46,15 +59,43 @@ namespace Exercice2
 
         /// <summary>
         /// ShoppingCart: permet de calculer le coût total des fruits (dans notre cas, ce sont les pommes et les oranges)
+        /// Cette méthode fait appelle à la méthode "Promo"
         /// </summary>
         /// <param name="panier">  liste donnée de fruits</param>
+        /// <param name="si_promo">permet de savoir si l'on applique la promotion sur les fruits ou pas</param>
         /// <returns></returns>
-        static double ShoppingCart(List<string> panier)
+        static double ShoppingCart(List<string> panier, Boolean si_promo)
         {
-            double prix_pommes = panier.Where(s => s == "pomme").Count() * prix_unite_pomme;
-            double prix_oranges = panier.Where(s => s == "orange").Count() * prix_unite_orange;
+            double prix_pommes = Promo(panier.Where(s => s == "pomme").Count(), si_promo ? 1 : 0, prix_unite_pomme);
+            double prix_oranges = Promo(panier.Where(s => s == "orange").Count(), si_promo ? 2 : 0, prix_unite_orange);
 
             return prix_pommes + prix_oranges;
+        }
+
+        /// <summary>
+        /// Promo: permet de calculer le coût total des fruits d'une même catégorie (pommes ou oranges)
+        /// </summary>
+        /// <param name="nb_fruits">liste de fruits d'une catégorie donnée</param>
+        /// <param name="k">indique le nombre de fruits à acheter pour pouvoir en avoir un gratuitement</param>
+        /// <param name="prix_unite_fruit">le prix unitaire d'un fruit d'une catégorie donnée</param>
+        /// <returns></returns>
+        static double Promo(int nb_fruits, int k, double prix_unite_fruit)
+        {
+            double total = 0; // le coût total des fruits d'une catégorie donnée
+
+            switch (k)
+            {
+                case 0: // Dans le cas où aucune promotion n'est appliquée
+                    total = nb_fruits * prix_unite_fruit;
+                    break;
+
+                default: // Dans le cas où une promotion est appliquée sur le fruit d'une catégorie donnée
+                    int nb_gratuit = nb_fruits / (k + 1);
+                    total = (nb_fruits - nb_gratuit) * prix_unite_fruit;
+                    break;
+            }
+
+            return total;
         }
     }
 }
